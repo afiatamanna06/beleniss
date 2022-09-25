b3f13f06cf71fd2c3c2085ac7040b56b49649f61
afiatamanna06 <afiatamanna06@gmail.com>
2022-09-26 00:01:23 +0600

    Add fonts

--- components/home/Intro.tsx
+++ components/home/Intro.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 
 function Intro() {
   return (
-    <Flex justify="center" position="relative" color="white" zIndex={3} h="100vh" bg="rgba(0, 0, 0, .7)">
+    <Flex justify="center" position="relative" color="white" zIndex={3} h="100vh" bg="rgba(0, 0, 0, .75)">
         <Center>
             <Flex justify="center" direction="column" alignItems="center">
-                <Box>Beleniss</Box>
-                <Box>Wedding Cake</Box>
+                <Box fontFamily="'Great Vibes', cursive" textAlign="center" fontSize="5xl" color="#C8A97E">Beleniss</Box>
+                <Box fontFamily="'Poppins', sans-serif" textAlign="center" letterSpacing={5} fontSize="6xl" fontWeight="semibold" textTransform="uppercase">Wedding Cake</Box>
             </Flex>
         </Center>
     </Flex>
