9ce8280c176f4896e3b1d1f3867d8f9d73dbf1e9
afiatamanna06 <afiatamanna06@gmail.com>
2022-09-26 11:41:24 +0600

    Update about section box sizing

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -8,7 +8,7 @@ function About() {
                 <Image src="/intro_1.jpg" w="20rem" h="33rem" alt="" />
                 <Image src="/intro_2.jpg" w="20rem" h="33rem" alt="" />
                 <Box w="20rem" h="33rem" display={["none", "none", "none", "flex", "flex"]}></Box>
-                <Flex border="2px" boxShadow="2xl" borderColor="#C8A97E" borderBottomLeftRadius="3xl" borderBottomRightRadius="3xl" direction="column" bg="white" p="16" top={[350, 350, 350, 20, 20]} left={[-10, -10, -10, 500, 500]} w={["25rem", "25rem", "25rem", "38rem", "38rem"]} position="absolute">
+                <Flex border="2px" boxShadow="2xl" borderColor="#C8A97E" borderBottomLeftRadius="3xl" borderBottomRightRadius="3xl" direction="column" bg="white" p="16" top={["30%", "30%", "30%", 20, 20]} left={[-6, -6, -6, 500, 500]} w={["23rem", "23rem", "23rem", "38rem", "38rem"]} position="absolute">
                     <Box fontFamily="'Poppins', sans-serif" letterSpacing={2} fontSize="3xl">About Us</Box>
                     <Box fontFamily="'Great Vibes', cursive" fontSize="4xl" mb="6" color="#9B4B15">Beautiful Story</Box>
                     <Box fontSize="sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, voluptatum, earum corrupti molestiae id, alias quaerat qui expedita voluptatibus sed magnam aperiam quos! Illum quo nobis repellat possimus alias reiciendis quas pariatur, labore maxime blanditiis. Nam perferendis veritatis necessitatibus? Animi distinctio at et assumenda doloremque possimus unde doloribus aliquid nobis.</Box>
