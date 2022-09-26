7993318ab7e798c6e48bae88789ef241302aadc6
afiatamanna06 <afiatamanna06@gmail.com>
2022-09-26 19:43:52 +0600

    Add galler section images

--- components/gallery/Gallery.tsx
+++ components/gallery/Gallery.tsx
@@ -1,10 +1,17 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, Grid, Image } from '@chakra-ui/react'
 import CommonHeader from '../common/CommonHeader'
+
+const galleryImages = ["/dish1.jpg", "/dish2.jpg", "/dish3.jpg", "/dish4.jpg", "/dish5.jpg", "/dish6.jpg", "/dish7.jpg", "/dish8.jpg"]
 
 function Gallery() {
   return (
-    <Flex direction="column" mt={[80, 80, 80, 0, 0]}>
+    <Flex direction="column" alignItems="center" mt={[80, 80, 80, 0, 0]} w="full">
         <CommonHeader heading="Gallery" description="Our Hot Picked Cakes" w={["full"]} />
+        <Grid w="full" alignItems="center" mt="20" templateColumns={["repeat(2, 1fr)", "repeat(2, 1fr)", "repeat(2, 1fr)", "repeat(3, 1fr)", "repeat(4, 1fr)"]}>
+            {galleryImages.map((image) => (
+                <Image objectFit="cover" w={["23rem", "23rem", "23rem", "19rem", "27rem"]} h="20rem" key={image} src={image} alt="" />
+            ))}
+        </Grid>
     </Flex>
   )
 }
