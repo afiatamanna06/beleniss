190afc2c5852523cc1538c5ea818861c5d1cc7b9
afiatamanna06 <afiatamanna06@gmail.com>
2022-09-27 20:01:19 +0600

    Add menu section

---
+++ components/menu/MenuCard.tsx
@@ -0,0 +1,26 @@
+import { Box, Flex, Image } from '@chakra-ui/react'
+import React from 'react'
+
+interface propTypes {
+    image: string;
+    name: string;
+    description: string;
+    price: string;
+}
+
+function MenuCard({image, name, description, price}: propTypes) {
+  return (
+    <Flex justify="space-between" alignItems="start" p="5" minW={["100%", "100%", "100%", "30rem", "30rem"]} border="1px" borderColor="gray.200" borderRadius="lg">
+        <Flex gap={4}>
+            <Image src={image} w="6rem" h="6rem" rounded="full" alt="" />
+            <Box>
+                <Box fontSize="2xl">{name}</Box>
+                <Box>{description}</Box>
+            </Box>
+        </Flex>
+        <Box fontWeight="bold" fontSize="lg">{price}</Box>
+    </Flex>
+  )
+}
+
+export default MenuCard
