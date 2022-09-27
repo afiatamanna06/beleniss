b90a123eb43d5c08f3a9cdf51a44170c1c89a05a
afiatamanna06 <afiatamanna06@gmail.com>
2022-09-27 19:25:53 +0600

    Add menu data

--- components/menu/Menu.tsx
+++ components/menu/Menu.tsx
@@ -5,6 +5,7 @@ function Menu() {
   return (
     <Flex id="menu" direction="column" alignItems="center" mt={[80, 80, 80, 0, 0]} pt="20" w="full">
         <CommonHeader heading="Specialities" description="Our Menu" w={["full"]} />
+        
     </Flex>
   )
 }
---
+++ components/menu/MenuData.ts
@@ -0,0 +1,56 @@
+export const menuData = [
+    {
+        image: "/menu1.jpg", 
+        name: "Strawberry",
+        description: "Strawberry, Chocolate, Berry, Cream",
+        price: "$20"
+    },
+    {
+        image: "/menu2.jpg", 
+        name: "Chocolate Cake",
+        description: "Strawberry, Chocolate, Berry, Cream",
+        price: "$30"
+    },
+    {
+        image: "/menu3.jpg", 
+        name: "Red Valvet",
+        description: "Strawberry, Chocolate, Berry, Cream",
+        price: "$35"
+    },
+    {
+        image: "/menu4.jpg", 
+        name: "Pineapple",
+        description: "Strawberry, Chocolate, Berry, Cream",
+        price: "$15"
+    },
+    {
+        image: "/menu5.jpg", 
+        name: "Belgium Cake",
+        description: "Strawberry, Chocolate, Berry, Cream",
+        price: "$30"
+    },
+    {
+        image: "/menu6.jpg", 
+        name: "Cookies",
+        description: "Strawberry, Chocolate, Berry, Cream",
+        price: "$10"
+    },
+    {
+        image: "/menu7.jpg", 
+        name: "Blackberries",
+        description: "Strawberry, Chocolate, Berry, Cream",
+        price: "$20"
+    },
+    {
+        image: "/menu8.jpg", 
+        name: "Orange",
+        description: "Strawberry, Chocolate, Berry, Cream",
+        price: "$25"
+    },
+    {
+        image: "/menu9.jpg", 
+        name: "Mango",
+        description: "Strawberry, Chocolate, Berry, Cream",
+        price: "$25"
+    },
+]
