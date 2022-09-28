2b76e7e491881130d38b2759a10c789a4edee477
afiatamanna06 <afiatamanna06@gmail.com>
2022-09-28 20:04:53 +0600

    Add testimonials list

---
+++ components/testimonials/TestimonialsList.ts
@@ -0,0 +1,28 @@
+const servicesList = [
+    {
+        image: "/person_1.png",
+        title: "Nilay Hirpara",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa illo corruptiratione voluptatibus recusandae dolore.", 
+        position: "Owner"
+    },
+    {
+        image: "/person_2.png",
+        title: "Nilay Hirpara",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa illo corruptiratione voluptatibus recusandae dolore.", 
+        position: "Owner"
+    },
+    {
+        image: "/person_3.png",
+        title: "Nilay Hirpara",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa illo corruptiratione voluptatibus recusandae dolore.", 
+        position: "Owner"
+    },
+    {
+        image: "/person_4.png",
+        title: "Nilay Hirpara",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa illo corruptiratione voluptatibus recusandae dolore.", 
+        position: "Owner"
+    },
+]
+
+export default servicesList
