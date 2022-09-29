4f9a6143dbdb3561fefb06f7ef6686cf520a3f5a
afiatamanna06 <afiatamanna06@gmail.com>
2022-09-29 19:33:12 +0600

    Add newsletter

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -6,9 +6,9 @@ import OpenHours from './OpenHours'
 
 function Footer() {
   return (
-    <Flex justify="center" bg="#141313" py="8">
+    <Flex justify="center" bg="#141313" pt={[14, 14, 14, 8]} pb="20" px={[8, 8, 8, 0, 0]}>
         <Center>
-            <Grid maxW="70rem" alignItems="start" gap={6} templateColumns={["repeat(2, 1fr)", "repeat(2, 1fr)", "repeat(2, 1fr)", "repeat(3, 1fr)", "repeat(4, 1fr)"]}>
+            <Grid maxW="70rem" alignItems="start" gap={7} templateColumns={["repeat(1, 1fr)", "repeat(1, 1fr)", "repeat(2, 1fr)", "repeat(2, 1fr)", "repeat(4, 1fr)"]}>
                 <FooterDescription />
                 <OpenHours />
                 <Instagram />
--- components/footer/Newsletter.tsx
+++ components/footer/Newsletter.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { Box, Button, Flex, Input } from '@chakra-ui/react'
 
 function Newsletter() {
   return (
-    <div>Newsletter</div>
+    <Flex direction="column" color="white" gap={6}>
+      <Box fontWeight="semibold" mt="5">Newsletter</Box>
+      <Flex direction="column">
+        <Box color="gray.300" fontSize="sm">
+          To get cake catalogue every month by subscribing below.
+        </Box>
+        <Input mt="4" bg="rgba(255, 255, 255, .1)" variant="filled" borderRadius="none" placeholder="example@gmail.com" _hover={{ bg: "rgba(255, 255, 255, .1)" }} _focus={{ borderWidth: "0px", bg: "rgba(255, 255, 255, .1)", borderColor: "#C8A97E", outline: "0", WebkitAppearance: "none", boxShadow: "none", WebkitBoxShadow: "none"  }} />
+        <Button mt="1" borderRadius="sm" bg="#C8A97E" color="white" _hover={{ bg: "#C8A97E" }} _focus={{ bg: "#C8A97E" }}>Subscribe</Button>
+      </Flex>
+    </Flex>
   )
 }
 
