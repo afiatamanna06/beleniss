c5794d5aa8ff3c03b30ce2ffa3ed210ad1215347
afiatamanna06 <afiatamanna06@gmail.com>
2022-09-29 19:15:44 +0600

    Add instagram

--- components/footer/Instagram.tsx
+++ components/footer/Instagram.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { Box, Flex, Grid, Image } from '@chakra-ui/react'
 
 function Instagram() {
   return (
-    <div>Instagram</div>
+    <Flex direction="column" color="white" gap={6}>
+      <Box fontWeight="semibold" mt="5">Instagram</Box>
+      <Grid alignItems="start" templateColumns={["repeat(3, 1fr)"]}>
+        <Image src="/insta-1.jpg" h="full" alt="" />
+        <Image src="/insta-2.jpg" h="full" alt="" />
+        <Image src="/insta-3.jpg" h="full" alt="" />
+        <Image src="/insta-4.jpg" h="full" alt="" />
+        <Image src="/insta-5.jpg" h="full" alt="" />
+        <Image src="/insta-6.jpg" h="full" alt="" />
+      </Grid>
+    </Flex>
   )
 }
 
