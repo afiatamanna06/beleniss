8bc62e1a54b9e07b9a4a24b091e9231ec59bc6c2
afiatamanna06 <afiatamanna06@gmail.com>
2022-12-10 22:27:07 +0600

    Fix menu cards styles

--- components/menu/MenuCard.tsx
+++ components/menu/MenuCard.tsx
@@ -10,10 +10,10 @@ interface propTypes {
 function MenuCard({image, name, description, price}: propTypes) {
   return (
     <Flex justify="space-between" cursor="pointer" _hover={{ border: "2px", borderColor: "#9B4B15" }} alignItems="start" p="5" minW={["100%", "100%", "100%", "100%", "10rem", "30rem"]} border="2px" borderColor="gray.100" borderRadius="lg">
-        <Flex gap={4}>
-            <Image src={image} w="6rem" h="6rem" rounded="full" alt="" />
+        <Flex gap={4} alignItems="start">
+            <Image src={image} w={["3rem", "3rem", "6rem", "6rem"]} h={["3rem", "3rem", "6rem", "6rem"]} rounded="full" alt="" />
             <Box>
-                <Box fontSize="2xl">{name}</Box>
+                <Box fontSize={["lg", "lg", "2xl", "2xl"]} fontWeight="semibold">{name}</Box>
                 <Box>{description}</Box>
             </Box>
         </Flex>
